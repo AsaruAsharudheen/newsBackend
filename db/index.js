d32dab513feae01901122ce104b3df16837f50ca
AsaruAsharudheen <asaru313313@gmail.com>
2025-08-01 10:24:48 +0530

    Your update message

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose
-  .connect('mongodb://localhost:27017/NewsWebsite') 
+  .connect(process.env.MONGO_URI) 
   .then(() => {
     console.log('DB CONNECTED');
   })
